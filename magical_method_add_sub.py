e61ba962218a4bba50a612f3b0ba2f2e844c1f80
Idris-mohammad <idriscmd2005@gmail.com>
2026-07-30 09:10:39 +0530

    drill

---
+++ magical_method_add_sub.py
@@ -0,0 +1,19 @@
+class Vector:
+    def __init__(self,a,b):
+        self.num=a
+        self.num2=b
+    def __add__(self, o2):
+        return self.num+o2.num,self.num2+o2.num2
+    def __sub__(self, o2):
+        return self.num-o2.num,self.num-o2.num2
+    def __str__(self):
+        return f"vector({self.num,self.num2})"
+    def __repr__(self):
+        return f"vector({self.num,self.num2})"
+v1=Vector(7,8)
+v2=Vector(6,7)
+print(v1+v2)
+print(v1-v2)
+print(v1)
+l=[v1,v2]
+print(l)
